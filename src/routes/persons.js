dc8c0e6b498433d163b059cf4cd6e3725792549b
vignesh222002 <gtavignesh22042002@gmail.com>
2023-12-03 09:39:43 +0530

    Completed CRED Operations

--- src/routes/persons.js
+++ src/routes/persons.js
@@ -19,7 +19,6 @@ router.get('/all', (request, response) => {
             response.status(500).send(err)
         }
         else {
-            console.log("Result: ", result)
             response.json(result)
         }
     })
@@ -27,40 +26,34 @@ router.get('/all', (request, response) => {
 
 router.post('/', (request, response) => {
     let persons = request.body.map((item) => `("${item.name}",${item.age})`).join(",");
-    console.log("Persons query", persons);
     db.query(`insert into persons (name, age) values ${persons};`)
     response.status(200).send("Persons Added Sucessfully")
 })
 
 router.put('/', async (request, response) => {
     await request.body.forEach(item => {
-        db.query(`update persons set name = "${item.name}", age = "${item.age}" where id = ${item.id}`, (err, result) => {
+        db.query(`update persons set name = "${item.name}", age = "${item.age}" where id = ${item.id};`, (err, result) => {
             if (err) {
                 console.log("Error in Mysql", err)
                 response.status(500).send(err)
             }
-            else {
-                response.status(200).send("Updated")
-            }
         })
     })
-    // db.query('select * form persons', (err, result) => {
-    //     if (err) {
-    //         console.log("Error in Mysql", err)
-    //         response.status(500).send(err)
-    //     }
-    //     else {
-    //         console.log("Result: ", result)
-    //         response.json(result)
-    //     }
-    // })
+    response.status(200).send("Updated");
 })
 
 
-// router.get('/:name', (request, response) => {
-//     const { name } = request.params;
-//     const person = personList.filter((item) => item.name == name);
-//     response.status(200).send(person)
-// })
+router.get('/:name', (request, response) => {
+    const { name } = request.params;
+    db.query(`select * from persons where name = "${name}";`, (err, result) => {
+        if (err) {
+            console.log("Error in Mysql", err)
+            response.status(500).send(err)
+        }
+        else {
+            response.status(200).send(result);
+        }
+    })
+})
 
 export default router; 
